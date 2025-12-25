eac2d8fd279bc56e3900081cf9139abcddc09d45
Diptodas123 <dasdipto2004@gmail.com>
2025-12-25 16:27:41 +0530

    Configure test environment with mock web environment

--- src/test/java/com/FindMyService/FindMyServiceApplicationTests.java
+++ src/test/java/com/FindMyService/FindMyServiceApplicationTests.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
 @ActiveProfiles("test")
 class FindMyServiceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		// This test verifies that the Spring application context loads successfully
 	}
 
 }
